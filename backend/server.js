05114ac760835fdf7cddbd2ca8e03bd8dca7d36c
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-26 21:38:26 +0530

    port env added

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,13 @@ import router from "./routes/product.route.js";
 const app = express();
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 app.use("/api/products", router)
 
-app.listen(5000, () => {
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:"+PORT);
 })
 
