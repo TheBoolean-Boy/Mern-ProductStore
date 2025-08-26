9805a5b182e4de2ecf1edbaa2d303f75510b4b63
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-26 21:25:48 +0530

    routes and controllers setup

--- backend/server.js
+++ backend/server.js
@@ -1,70 +1,12 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
+
+import router from "./routes/product.route.js";
 
 const app = express();
 app.use(express.json());
 
-
-app.get("/api/products", async(req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(201).json({success:true, data: products })
-  } catch (error) {
-    console.log("Error is fetching products", error.message);
-    res.status(500).json({success:false, message:"Server Error"})
-  }
-})
-
-app.post("/api/products", async(req, res) => {
-  const product = req.body;
-
-  if(!product.name || !product.image || !product.price){
-    return res.status(400).json({success: false, message: "Please Provide all the fields"})
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({success:true, data:newProduct})
-  } catch (error) {
-    console.log("Error creating doc", error.message);
-    res.status(500).json({success: false, message: "Internal Server Error"})
-  }
-
-});
-
-app.delete("/api/products/:id", async(req, res) => {
-  const {id} = req.params;
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(201).json({success: true, message: "Product deleted"})
-  } catch (error) {
-    console.log("Error in removing product", error.message)
-    res.status(404).json({success: false, message: "Product not found"})
-  }
-})
-
-app.put("/api/products/:id", async(req, res) => {
-  const {id} = req.params;
-
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json({success:false, message: "No such Product Found"});
-  }
-
-  const toUpdate = req.body;
-
-  try {
-    const updatedProduct  = await Product.findByIdAndUpdate(id, toUpdate, {new:true});
-    res.status(200).json({success: true, data:updatedProduct})
-  } catch (error) {
-    console.log("Error updating product", error.message);
-    res.status(500).json({success:true, message:"Server Error"})
-  }
-})
+app.use("/api/products", router)
 
 app.listen(5000, () => {
   connectDB();
