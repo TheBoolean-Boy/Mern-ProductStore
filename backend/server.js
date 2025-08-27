af5feb29172aef2b89c6464a0346f1943c77e9ab
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-27 10:32:08 +0530

    Production configuration added

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-
+import path, { dirname } from 'path'
 import router from "./routes/product.route.js";
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/products", router)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 
 app.listen(PORT, () => {
