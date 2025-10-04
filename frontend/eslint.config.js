c3cdf0a3e8724c055e82845e318c714a6183d928
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-10-04 12:30:58 +0530

    refactor: modified old code with better modern practices

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+const config = defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -26,4 +25,6 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-])
+]);
+
+export default config;
